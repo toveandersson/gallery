b2a2498440866480f081ed43cefb95f675010af6
toveandersson <toveama@gmail.com>
2024-10-21 19:01:29 +0200

    test2

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+const port = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-const port = 3001
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`)
 })
