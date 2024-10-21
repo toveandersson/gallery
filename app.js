04191c8f5938c046a0cbbf8e4651ab6fb35b4909
toveandersson <toveama@gmail.com>
2024-10-21 19:51:07 +0200

    test3

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`)
 })
