490cfae4036da75c3ce388d6270dffd3717d0141
toveandersson <toveama@gmail.com>
2025-03-03 03:29:55 +0100

    upd

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ if (!STRIPE_KEY) {
 }
 const stripe = require('stripe')(STRIPE_KEY);
 
-//const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
+const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
 // const lineItems = await stripe.checkout.sessions.listLineItems(
 //   'cs_test_a1enSAC01IA3Ps2vL32mNoWKMCNmmfUGTeEeHXI5tLCvyFNGsdG2UNA7mr'
 // );
@@ -251,59 +251,59 @@ app.post('/create-checkout-session', async (req, res) => {
   }
 });
 
-// app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
-//   let event = request.body;
-//   // Only verify the event if you have an endpoint secret defined.
-//   // Otherwise use the basic event deserialized with JSON.parse
-//   if (endpointSecret) {
-//     // Get the signature sent by Stripe
-//     const signature = request.headers['stripe-signature'];
-//     try {
-//       event = stripe.webhooks.constructEvent(
-//         request.body,
-//         signature,
-//         endpointSecret
-//       );
-//     } catch (err) {
-//       console.log(`⚠️  Webhook signature verification failed.`, err.message);
-//       return response.sendStatus(400);
-//     }
-//   }
+app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+  let event = request.body;
+  // Only verify the event if you have an endpoint secret defined.
+  // Otherwise use the basic event deserialized with JSON.parse
+  if (endpointSecret) {
+    // Get the signature sent by Stripe
+    const signature = request.headers['stripe-signature'];
+    try {
+      event = stripe.webhooks.constructEvent(
+        request.body,
+        signature,
+        endpointSecret
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`, err.message);
+      return response.sendStatus(400);
+    }
+  }
 
-//   console.log(`Received event of type: ${event.type}`);
+  console.log(`Received event of type: ${event.type}`);
 
-//   // Handle the event
-//   switch (event.type) {
-//     case 'payment_intent.succeeded':
-//       const paymentIntent = event.data.object;
-//       console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
-//       console.log("Sending email for poster:");
-//       //const lineItems = await stripe.checkout.sessions.listLineItems(paymentIntent.session.id);
-//       console.log(lineItems);
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntent = event.data.object;
+      console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+      console.log("Sending email for poster:");
+      //const lineItems = await stripe.checkout.sessions.listLineItems(paymentIntent.session.id);
+      console.log(lineItems);
 
 
 
-//       sendMail(
-//         process.env.MONGO_USER,
-//         `Hello ${paymentIntent.name}!`,
-//         `You have purchased: ${lineItems}`,
-//         "Thank you :-)"
-//       );
-//       // Then define and call a method to handle the successful payment intent.
-//       // handlePaymentIntentSucceeded(paymentIntent);
-//       break;
-//     case 'payment_method.attached':
-//       const paymentMethod = event.data.object;
-//       // Then define and call a method to handle the successful attachment of a PaymentMethod.
-//       // handlePaymentMethodAttached(paymentMethod);
-//       break;
-//     default:
-//       // Unexpected event type
-//       console.log(`Unhandled event type ${event.type}.`);
-//   }
+      sendMail(
+        process.env.MONGO_USER,
+        `Hello ${paymentIntent.name}!`,
+        `You have purchased: ${lineItems}`,
+        "Thank you :-)"
+      );
+      // Then define and call a method to handle the successful payment intent.
+      // handlePaymentIntentSucceeded(paymentIntent);
+      break;
+    case 'payment_method.attached':
+      const paymentMethod = event.data.object;
+      // Then define and call a method to handle the successful attachment of a PaymentMethod.
+      // handlePaymentMethodAttached(paymentMethod);
+      break;
+    default:
+      // Unexpected event type
+      console.log(`Unhandled event type ${event.type}.`);
+  }
 
-//   // Return a 200 response to acknowledge receipt of the event
-//   response.send();
-// });
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
 
 start();
